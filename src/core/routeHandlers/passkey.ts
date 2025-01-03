e5b123957aaa88cc366062f68963e16e3411bd59
sourabpramanik <shubpramanik241@gmail.com>
2025-01-03 16:54:48 +0000

    chore: cleanup

--- src/core/routeHandlers/passkey.ts
+++ src/core/routeHandlers/passkey.ts
@@ -3,15 +3,14 @@ import { InvalidPasskeyRequest } from '../error'
 import { GeneratePasskeyRegistration } from '../protocols/passkey'
 
 export function PasskeyHandlers(
-  request: PayloadRequest,
-  resource: string,
-  rpID: string,
-  //   sessionCallBack: (oauthAccountInfo: OAuthAccountInfo) => Promise<Response>,
+    request: PayloadRequest,
+    resource: string,
+    rpID: string,
 ): Promise<Response> {
-  switch (resource) {
-    case 'generate-registration-options':
-      return GeneratePasskeyRegistration(request, rpID)
-    default:
-      throw new InvalidPasskeyRequest()
-  }
+    switch (resource) {
+        case 'generate-registration-options':
+            return GeneratePasskeyRegistration(request, rpID)
+        default:
+            throw new InvalidPasskeyRequest()
+    }
 }
