4fc5c438de0e7aedd3bdc0eecd26403c94e537a2
sourabpramanik <shubpramanik241@gmail.com>
2025-01-05 17:14:35 +0000

    feat: new err

--- src/core/errors/apiErrors.ts
+++ src/core/errors/apiErrors.ts
@@ -1,6 +1,6 @@
 import { APIError } from "payload";
 
-class AuthAPIError extends APIError {
+export class AuthAPIError extends APIError {
     constructor(message: string, status?: number, data?: Record<string, unknown> | undefined) {
         super(message, status ?? 400, data, true)
     }
@@ -9,5 +9,11 @@ class AuthAPIError extends APIError {
 export class MissingEmailAPIError extends AuthAPIError {
     constructor() {
         super("Missing email. Email is required")
+    }
+}
+
+export class EmailNotFoundAPIError extends AuthAPIError {
+    constructor() {
+        super("Now user found with this email", 404)
     }
 }
