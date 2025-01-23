9aaae3ff7edd4fce8d529f32408d8ab754002494
sourabpramanik <shubpramanik241@gmail.com>
2025-01-23 04:08:12 +0000

    chore: fix script

--- script/changelog.sh
+++ script/changelog.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-PACKAGE_VERSION=$(node -p -e "require('../package.json').version")
-bun x git-cliff -o '../CHANGELOG.md' --tag $PACKAGE_VERSION
+# Get the package version from package.json
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version || ''")
+
+# Check if PACKAGE_VERSION is empty
+if [ -z "$PACKAGE_VERSION" ]; then
+  echo "Error: Failed to retrieve version from package.json"
+  exit 1
+fi
+
+# Run git-cliff with the correct tag
+bun x git-cliff -o './CHANGELOG.md' --tag "$PACKAGE_VERSION"
