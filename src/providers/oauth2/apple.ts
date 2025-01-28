2bb45dcda5c534db2773a42b734620e004bcf876
sourabpramanik <shubpramanik241@gmail.com>
2025-01-28 15:27:23 +0000

    fix: pass response mode param

--- src/providers/oauth2/apple.ts
+++ src/providers/oauth2/apple.ts
@@ -23,6 +23,10 @@ function AppleOAuth2Provider(config: AppleAuthConfig): OAuth2ProviderConfig {
     authorization_server,
     name: "Apple",
     algorithm,
+    params: {
+      ...config.params,
+      response_mode: "form_post",
+    },
     profile: (profile): AccountInfo => {
       return {
         sub: profile.sub as string,
