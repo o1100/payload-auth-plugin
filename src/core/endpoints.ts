4cde625d20619ebacf84807ef57a4b8aee0c564c
sourabpramanik <shubpramanik241@gmail.com>
2025-02-13 16:12:34 +0000

    feat: updated endpoint strategy

--- src/core/endpoints.ts
+++ src/core/endpoints.ts
@@ -1,22 +1,37 @@
 import type { BasePayload, Endpoint, PayloadRequest } from "payload"
-import type {
-  AccountInfo,
-  OAuthProviderConfig,
-  ProvidersConfig,
-} from "../types.js"
+import type { AccountInfo, OAuthProviderConfig } from "../types.js"
 import { OAuthHandlers } from "./routeHandlers/oauth.js"
 import { PasskeyHandlers } from "./routeHandlers/passkey.js"
 
-export class EndpointFactory {
-  readonly #providers: Record<string, ProvidersConfig>
-  readonly #payloadOAuthPath: string = "/admin/oauth/:resource/:provider"
-  readonly #payloadPasskeyPath: string = "/admin/passkey/:resource"
-  constructor(providers: Record<string, ProvidersConfig>) {
-    this.#providers = providers
-  }
-  payloadOAuthEndpoints({
+/**
+ * Base interface for all endpoint strategies. Useful to keep extending for providers with
+ * different requirements to interact with
+ *
+ * @interface EndpointStrategy
+ *
+ * @typedef {EndpointStrategy}
+ *
+ */
+interface EndpointStrategy {
+  createEndpoints(config: any): Endpoint[]
+}
+
+/**
+ * Oauth endpoint strategy to implement dynamic enpoints for all type of Oauth providers
+ *
+ * @export
+ * @class OAuthEndpointStrategy
+ * @typedef {OAuthEndpointStrategy}
+ * @internal
+ */
+export class OAuthEndpointStrategy implements EndpointStrategy {
+  constructor(private providers: Record<string, OAuthProviderConfig>) {}
+
+  createEndpoints({
+    pluginType,
     sessionCallback,
   }: {
+    pluginType: string
     sessionCallback: (
       oauthAccountInfo: AccountInfo,
       scope: string,
@@ -26,10 +41,10 @@ export class EndpointFactory {
   }): Endpoint[] {
     return [
       {
-        path: this.#payloadOAuthPath,
+        path: `${pluginType}/oauth/:resource/:provider`,
         method: "get",
         handler: (request: PayloadRequest) => {
-          const provider = this.#providers[
+          const provider = this.providers[
             request.routeParams?.provider as string
           ] as OAuthProviderConfig
 
@@ -50,10 +65,24 @@ export class EndpointFactory {
       },
     ]
   }
-  payloadPasskeyEndpoints({
+}
+
+/**
+ * Passkey endpoint strategy to implement enpoints for Passkey provider
+ *
+ * @export
+ * @class PasskeyEndpointStrategy
+ * @typedef {PasskeyEndpointStrategy}
+ * @implements {EndpointStrategy}
+ * @internal
+ */
+export class PasskeyEndpointStrategy implements EndpointStrategy {
+  createEndpoints({
+    pluginType,
     rpID,
     sessionCallback,
   }: {
+    pluginType: string
     rpID: string
     sessionCallback: (
       accountInfo: AccountInfo,
@@ -63,7 +92,7 @@ export class EndpointFactory {
   }): Endpoint[] {
     return [
       {
-        path: this.#payloadPasskeyPath,
+        path: `${pluginType}/passkey/:resource`,
         method: "post",
         handler: (request: PayloadRequest) => {
           return PasskeyHandlers(
@@ -77,5 +106,36 @@ export class EndpointFactory {
         },
       },
     ]
+  }
+}
+
+/**
+ * The generic endpoint factory class
+ *
+ * @export
+ * @class EndpointsFactory
+ * @typedef {EndpointsFactory}
+ * @internal
+ */
+export class EndpointsFactory {
+  private strategies: Record<string, EndpointStrategy> = {}
+  constructor(private pluginType: "admin" | "app") {}
+
+  registerStrategy(
+    name: "oauth" | "passkey",
+    strategy: EndpointStrategy,
+  ): void {
+    this.strategies[name] = strategy
+  }
+
+  createEndpoints(
+    strategyName: "oauth" | "passkey",
+    config?: any | undefined,
+  ): Endpoint[] {
+    const strategy = this.strategies[strategyName]
+    if (!strategy) {
+      throw new Error(`Strategy "${strategyName}" not found.`)
+    }
+    return strategy.createEndpoints({ pluginType: this.pluginType, ...config })
   }
 }
