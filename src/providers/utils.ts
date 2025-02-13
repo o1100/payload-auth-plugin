6efb56bdfd437a9c53a2179982e7e236825b12e1
sourabpramanik <shubpramanik241@gmail.com>
2025-02-13 16:13:26 +0000

    feat: new provider utils

--- src/providers/utils.ts
+++ src/providers/utils.ts
@@ -1,19 +1,56 @@
 import { ProviderAlreadyExists } from "../core/errors/consoleErrors.js"
-import { ProvidersConfig } from "../types.js"
+import {
+  ProvidersConfig,
+  OAuthProviderConfig,
+  PasskeyProviderConfig,
+} from "../types.js"
 
-export function mapProviders(providers: ProvidersConfig[]) {
+/**
+ * Reducer function to extract the OAuth providers
+ *
+ * @internal
+ * @param {ProvidersConfig[]} providers
+ * @returns {Record<string, OAuthProviderConfig>}
+ */
+export function getOAuthProviders(
+  providers: ProvidersConfig[],
+): Record<string, OAuthProviderConfig> {
   const providerRecords = providers.reduce(
-    (record: Record<string, ProvidersConfig>, provider: ProvidersConfig) => {
+    (
+      record: Record<string, OAuthProviderConfig>,
+      provider: ProvidersConfig,
+    ) => {
       if (record[provider.id]) {
         throw new ProviderAlreadyExists()
       }
       const newRecord = {
         ...record,
-        [provider.id]: provider,
+      }
+      if (provider.kind === "oauth") {
+        newRecord[provider.id] = provider
       }
       return newRecord
     },
     {},
   )
   return providerRecords
+}
+
+/**
+ * Function to get the Passkey provider
+ *
+ * @export
+ * @param {ProvidersConfig[]} providers
+ * @returns {(PasskeyProviderConfig | null)}
+ */
+export function getPasskeyProvider(
+  providers: ProvidersConfig[],
+): PasskeyProviderConfig | null {
+  const passkeyProvider = providers.find(
+    (provider) => provider.kind === "passkey",
+  )
+  if (passkeyProvider) {
+    return passkeyProvider
+  }
+  return null
 }
