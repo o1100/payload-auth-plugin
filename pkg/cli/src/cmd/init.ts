c07ab7f752ba15f9486936984b95e13b82d502eb
sourabpramanik <shubpramanik241@gmail.com>
2025-02-15 09:29:22 +0000

    feat: create accounts collection

--- pkg/cli/src/cmd/init.ts
+++ pkg/cli/src/cmd/init.ts
@@ -1,9 +1,12 @@
+import fs from "fs-extra"
 import { Command } from "commander"
 import { logger } from "../utils/logger.js"
 import { handleError } from "../utils/error.js"
 import prompts from "prompts"
 import * as v from "valibot"
 import { intallDeps } from "../utils/dependencies.js"
+import path from "path"
+import { accountsCollection } from "../config/collections/accounts.js"
 
 const initOptionsSchema = v.object({
   cwd: v.string(),
@@ -25,7 +28,36 @@ export const initCommand = new Command()
 
       const options = v.parse(initOptionsSchema, incomingOptions)
 
-      await setup(options)
+      // Prompt for plugin
+      const { pluginType } = await prompts({
+        type: "select",
+        name: "pluginType",
+        message: "Which plugin you want to set up?",
+        choices: [
+          {
+            title: "App authentication plugin",
+            description:
+              "This plugin is used for implementing authentication only to the frontend of Payload project.",
+            value: "app",
+          },
+          {
+            title: "Admin authentication plugin",
+            description:
+              "This plugin is used for implementing authentication only to the Admin of Payload project.",
+            value: "admin",
+          },
+        ],
+      })
+
+      if (pluginType === "app") {
+        await setupApp(options)
+      } else if (pluginType === "admin") {
+        await setupAdmin(options)
+      } else {
+        logger.break()
+        logger.error("Wrong plugin type")
+        process.exit(1)
+      }
 
       logger.break()
     } catch (error) {
@@ -34,12 +66,12 @@ export const initCommand = new Command()
     }
   })
 
-async function setup(options: v.InferInput<typeof initOptionsSchema>) {
+async function setupApp(options: v.InferInput<typeof initOptionsSchema>) {
   // Prompt for Payload plugin file path
   const { pluginsFilePath } = await prompts({
     type: "text",
     name: "pluginsFilePath",
-    message: "Path to the Payload plugins files.",
+    message: "Path to the Payload plugins files:",
     initial: "src/plugins/index.ts",
   })
 
@@ -47,33 +79,81 @@ async function setup(options: v.InferInput<typeof initOptionsSchema>) {
   const { collectionsDir } = await prompts({
     type: "text",
     name: "collectionsDir",
-    message: "Path to the Payload collections directory.",
+    message: "Path to the Payload collections directory:",
     initial: "src/collections",
   })
 
-  // Prompt for plugin
-  const { pluginType } = await prompts({
-    type: "select",
-    name: "pluginType",
-    message: "Which plugin you want to set up?",
-    choices: [
-      {
-        title: "App authentication plugin",
-        description:
-          "This plugin is used for implementing authentication only to the frontend of Payload project.",
-        value: "app",
-      },
-      {
-        title: "Admin authentication plugin",
-        description:
-          "This plugin is used for implementing authentication only to the Admin of Payload project.",
-        value: "admin",
-      },
-    ],
+  // Prompt for Payload plugin file path
+  const { accountsCollectionSlug } = await prompts({
+    type: "text",
+    name: "accountsCollectionSlug",
+    message: "What should be the slug for the accounts collection:",
+    initial: "accounts",
   })
 
   // Installing dependencies
-  await intallDeps(options.cwd, options.silent)
+  //   await intallDeps(options.cwd, options.silent)
 
+  if (fs.existsSync(path.resolve(options.cwd, collectionsDir, ""))) {
+    logger.break()
+    logger.error("The Auth collection directory already exists.")
+    process.exit(1)
+  }
+  fs.mkdir(path.resolve(options.cwd, collectionsDir, "Auth"))
+  logger.info("")
+}
+
+async function setupAdmin(options: v.InferInput<typeof initOptionsSchema>) {
+  // Prompt for Payload plugin file path
+  const { pluginsFilePath } = await prompts({
+    type: "text",
+    name: "pluginsFilePath",
+    message: "Path to the Payload plugins files:",
+    initial: "src/plugins/index.ts",
+  })
+
+  // Prompt for Payload plugin file path
+  const { collectionsDir } = await prompts({
+    type: "text",
+    name: "collectionsDir",
+    message: "Path to the Payload collections directory:",
+    initial: "src/collections",
+  })
+
+  // Prompt for Payload plugin file path
+  const { accountsCollectionSlug } = await prompts({
+    type: "text",
+    name: "accountsCollectionSlug",
+    message: "What should be the slug for the accounts collection:",
+    initial: "accounts",
+  })
+
+  if (
+    fs.existsSync(
+      path.resolve(options.cwd, collectionsDir, "Accounts/index.ts"),
+    )
+  ) {
+    logger.break()
+    logger.warn("Looks like Accounts collection already exists.")
+    const { overwriteAccountsCollection } = await prompts({
+      type: "confirm",
+      name: "overwriteAccountsCollection",
+      message: "Do you want to overwrite the existing Accounts collection?",
+    })
+    if (overwriteAccountsCollection) {
+      fs.removeSync(
+        path.resolve(options.cwd, collectionsDir, "Accounts/index.ts"),
+      )
+    } else {
+      process.exit(1)
+    }
+  }
+  await fs.createFile(
+    path.resolve(options.cwd, collectionsDir, "Accounts/index.ts"),
+  )
+  await fs.writeFile(
+    path.resolve(options.cwd, collectionsDir, "Accounts/index.ts"),
+    accountsCollection(accountsCollectionSlug),
+  )
   logger.info("")
 }
--- pkg/pulgin/src/core/collections/admin/accounts.ts
+++ pkg/pulgin/src/core/collections/admin/accounts.ts
@@ -1,5 +1,38 @@
 import { CollectionConfig } from "payload"
-import { PasskeyFields } from "./accounts/passkeyFields.js"
+import { Field } from "payload"
+
+export const PasskeyFields: Field[] = [
+  {
+    name: "credentialId",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "publicKey",
+    type: "json",
+    required: true,
+  },
+  {
+    name: "counter",
+    type: "number",
+    required: true,
+  },
+  {
+    name: "transports",
+    type: "json",
+    required: true,
+  },
+  {
+    name: "deviceType",
+    type: "text",
+    required: true,
+  },
+  {
+    name: "backedUp",
+    type: "checkbox",
+    required: true,
+  },
+]
 
 export function buildAccountsCollection(
   account: {
