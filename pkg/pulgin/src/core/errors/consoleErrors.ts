d86c4a393a62518b90951259d6f8a3ff110497ec
sourabpramanik <shubpramanik241@gmail.com>
2025-02-25 18:29:52 +0000

    feat: new exceptions

--- pkg/pulgin/src/core/errors/consoleErrors.ts
+++ pkg/pulgin/src/core/errors/consoleErrors.ts
@@ -1,4 +1,4 @@
-class AuthError extends Error {
+class PluginError extends Error {
   constructor(message: string, cause?: string) {
     super(message)
     this.name = "PAYLOAD_AUTH_PLUGIN_ERROR"
@@ -7,26 +7,26 @@ class AuthError extends Error {
     this.stack = ""
   }
 }
-export class InvalidServerURL extends AuthError {
+export class InvalidServerURL extends PluginError {
   constructor() {
     super(
       "Missing or invalid server URL. Please set serverURL in your Payload config",
     )
   }
 }
-export class InvalidProvider extends AuthError {
+export class InvalidProvider extends PluginError {
   constructor() {
     super("Invalid Provider")
   }
 }
 
-export class ProviderAlreadyExists extends AuthError {
+export class ProviderAlreadyExists extends PluginError {
   constructor() {
     super("Duplicate provider found")
   }
 }
 
-export class InvalidOAuthAlgorithm extends AuthError {
+export class InvalidOAuthAlgorithm extends PluginError {
   constructor() {
     super(
       "Invalid OAuth Algorithm. Plugin only support OIDC and OAuth2 algorithms",
@@ -34,50 +34,62 @@ export class InvalidOAuthAlgorithm extends AuthError {
   }
 }
 
-export class InvalidOAuthResource extends AuthError {
+export class InvalidOAuthResource extends PluginError {
   constructor() {
     super("Invalid resource request. Check docs before initiating requests")
   }
 }
 
-export class MissingOrInvalidSession extends AuthError {
+export class MissingOrInvalidSession extends PluginError {
   constructor() {
     super("Missing or invalid session.")
   }
 }
 
-export class MissingOrInvalidParams extends AuthError {
+export class MissingOrInvalidParams extends PluginError {
   constructor() {
     super("Missing or invalid params")
   }
 }
 
-export class AuthenticationFailed extends AuthError {
+export class AuthenticationFailed extends PluginError {
   constructor() {
     super("Failed to authenticate")
   }
 }
 
-export class UserNotFound extends AuthError {
+export class UserNotFound extends PluginError {
   constructor() {
     super("User not found")
   }
 }
 
-export class InvalidCredentials extends AuthError {
+export class InvalidCredentials extends PluginError {
   constructor() {
     super("Invalid credentials")
   }
 }
 
-export class MissingUsersCollection extends AuthError {
+export class MissingUsersCollection extends PluginError {
   constructor() {
     super("Missing users collection")
   }
 }
 
-export class InvalidPasskeyRequest extends AuthError {
+export class InvalidPasskeyRequest extends PluginError {
   constructor() {
     super("Invalid or missing request")
+  }
+}
+
+export class InvalidCollectionSlug extends PluginError {
+  constructor() {
+    super("Missing or invalid collection slug")
+  }
+}
+
+export class MissingCollections extends PluginError {
+  constructor() {
+    super("Missing collections")
   }
 }
