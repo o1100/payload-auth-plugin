43bb90278f9b1bd650a98666fad3fb44a1f38ba6
sourabpramanik <shubpramanik241@gmail.com>
2025-03-12 16:22:52 +0000

    fix: refresh response

--- src/core/protocols/session.ts
+++ src/core/protocols/session.ts
@@ -1,6 +1,7 @@
 import { parseCookies, PayloadRequest } from "payload"
 import { UnauthorizedAPIRequest } from "../errors/apiErrors.js"
 import { createSessionCookies, verifySessionCookie } from "../utils/cookies.js"
+import { SuccessKind } from "../../types.js"
 
 export async function SessionRefresh(
   name: string,
@@ -22,9 +23,12 @@ export async function SessionRefresh(
     ...(await createSessionCookies(name, secret, jwtResponse.payload)),
   ]
 
-  const res = new Response(JSON.stringify({ mesage: "Session refreshed" }), {
-    status: 201,
-  })
+  const res = new Response(
+    JSON.stringify({ message: "Session refreshed", kind: SuccessKind.Updated }),
+    {
+      status: 201,
+    },
+  )
   refreshCookies.forEach((cookie) => {
     res.headers.append("Set-Cookie", cookie)
   })
