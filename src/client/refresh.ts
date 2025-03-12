20cc043dd971a4e303f3c6a1c6c226bc21159493
sourabpramanik <shubpramanik241@gmail.com>
2025-03-12 16:21:55 +0000

    fix: output

--- src/client/refresh.ts
+++ src/client/refresh.ts
@@ -1,33 +1,17 @@
-import {
-  ErrorKind,
-  CallbackError,
-  SuccessKind,
-  CallbackSuccess,
-} from "../types.js"
+import { AuthPluginOutput } from "../types.js"
 
 interface BaseOptions {
   name: string
 }
-export interface ExtendedRefreshOptions {
-  onError?: (args: CallbackError) => unknown | undefined
-  onSuccess?: (args: CallbackSuccess) => unknown | undefined
-}
 export const refresh = async (
   options: BaseOptions,
-  extendedOpts?: ExtendedRefreshOptions,
-) => {
-  const base = process.env.NEXT_PUBLIC_PAYLOAD_AUTH_URL
+): Promise<AuthPluginOutput> => {
+  const base = process.env.NEXT_PUBLIC_SERVER_URL
   const response = await fetch(`${base}/api/${options.name}/session/refresh`)
-  const message = await response.json()
-  if (!response.ok) {
-    if (extendedOpts?.onError) {
-      // TODO fix the logic
-      extendedOpts.onError({ message, kind: ErrorKind.Unauthorized })
-      return
-    }
+  const { message, kind, data } = (await response.json()) as AuthPluginOutput
+  return {
+    message,
+    kind,
+    data,
   }
-  if (extendedOpts?.onSuccess) {
-    extendedOpts?.onSuccess({ message, kind: SuccessKind.Created })
-  }
-  return
 }
