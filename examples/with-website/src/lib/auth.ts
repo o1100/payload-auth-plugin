0e17a16c16f5fe41fe9de256f9fb7f8b340e2e8a
sourabpramanik <shubpramanik241@gmail.com>
2025-04-07 23:45:20 +0000

    chore: cleanup

--- examples/with-website/src/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { appClient } from 'payload-auth-plugin/client'
-
-const { signin } = appClient({ name: 'app' })
-
-export const handleOauthSignin = (provider: 'google' | 'github') => {
-  signin().oauth(provider)
-}
